b78df3b633ec8d607740e2feb84cc504ad3e3a63
ilan vol <ilanvol96@gmail.com>
2021-08-27 21:03:55 +0300

    added welcoming messge trhat asks they players if they want to play, and a "bye" messege

--- TicTacToe/prog.c
+++ TicTacToe/prog.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
 /**
 	This is the first rpogram im uploading to GitHub
 	So I decided to mamke a tix-tac-toe game for my time
 	Creator: Ilan Vol
-
 **/
 
+#define FALSE 0
+#define TRUE !FALSE
 #define ROWS 3
 #define COLS 3
 
+bool startGame();
 int displayBoard(char board[][COLS]);
 
 int main()
 {
-	char board[ROWS][COLS] = { {'7','8','9'},{'4','5','6'},{'1','2','3'} };
-
+	bool toPlay = FALSE;
+	char board[ROWS][COLS] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+	toPlay=startGame(board);
+	//while(toPlay)
 	displayBoard(board);
+	puts("Bye-bye");
+	system("pause");
 	return 0;
 }
 
 
 /*
+	printf a B E A Utiful welcoming messege for the players and ask if they want to play
+	input:  none
+	output: FALSE - if players dont want to play
+	        TRUE  - if players want to play
+*/
+bool startGame()
+{
+	bool toPlay = FALSE;
+	char answer = '0';
+
+	system("cls");
+	puts("Welcome players!");
+	puts("This is a game of Tic-Tac-Toe");
+	puts("As you will see in the board below, it is as the numpad on the right of your keyboard");
+	puts("Do you wish to play?");
+	puts("click y or Y to play and anything else to quit the game");
+	answer = _getch();
+	printf("%c\n",answer);
+	if (answer == 'y' || answer == 'Y')
+	{
+		puts("Lets play Tic-Tac-Toe!");
+		toPlay = TRUE;
+	}
+	return toPlay;
+}
+
+/*
 	Function will display game-board
+	input: two dim array - board
+	output: 0 on success
 */
 int displayBoard(char board[][COLS])
 {
@@ -44,6 +81,5 @@ int displayBoard(char board[][COLS])
 		printf("\n|     |     |     |\n");
 	}
 	printf("-------------------\n");
-
 	return 0;
 }
