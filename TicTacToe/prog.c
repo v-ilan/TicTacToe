fe359af60a226f7a9749b00e071babc9bb6d4792
ilan vol <ilanvol96@gmail.com>
2021-08-28 15:49:48 +0300

    instead of fixinbg the problem that 'doTurn' returned 1-2 instead of 0-1 i used it, now the actual player num is used instead of represntation
    created 'checkWinner' function that checks if last player to play won the game. still needs testing
    
    added anEnd function that announces who won

--- TicTacToe/prog.c
+++ TicTacToe/prog.c
@@ -12,26 +12,34 @@
 #define ROWS 3
 #define COLS 3
 #define NUM_PLAYERS 2
+#define NUM_ROUNDS 9
 
 int startGame();
-int checkWinner(char board[][COLS]);
-int doTurn(char board[][COLS], int player);
-int displayBoard(char board[][COLS]);
-
+int anEnd(int gameStop);
+int checkWinner(char board[][COLS], int player, const int  ROW_SIZE);
+int doTurn(char board[][COLS], int player, const int ROW_SIZE);
+int displayBoard(char board[][COLS], const int  ROW_SIZE);
+/*
+* 
+*/
 int main()
 {
-	int stopGame = FALSE;
+	int stopGame = FALSE, rounds = NUM_ROUNDS;
 	char board[ROWS][COLS] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-	int player = 0;
+	int player = NUM_PLAYERS;
 	stopGame =startGame(board);
-	while (!stopGame)
+	while (rounds && !stopGame)
 	{
 		system("cls");
 		puts("Lets play Tic-Tac-Toe!");
-		displayBoard(board);
-		stopGame = checkWinner(board);
-		player = doTurn(board, player);
+		displayBoard(board, ROWS);
+		rounds--;
+		player=doTurn(board, player, ROWS);
+		stopGame = checkWinner(board, player, ROWS);
 	}
+	system("cls");
+	displayBoard(board, ROWS);
+	anEnd(stopGame);
 	puts("Bye-bye");
 	system("pause");
 	return 0;
@@ -65,15 +73,111 @@ int startGame()
 }
 
 /*
+	Function will announce who won the game or tell if no one did
+	input:	gameStop - indication of winner
+	output: 0 on success
+*/
+int anEnd(int gameStop)
+{
+
+	if (gameStop)
+	{
+		printf("player %d hsa won the game!\n", gameStop);
+	}
+	else
+	{
+		puts("No one has one the game");
+	}
+	return 0;
+}
+
+/*
 	function checks if last player won
 	input: gamaing board, player num
 	output: 0 - if no player won
 			1 - if player1 won
 			2 - if player2 won
 */
-int checkWinner(char board[][COLS])
+int checkWinner(char board[][COLS], int player, const int  ROW_SIZE)
 {
-	return 0;
+	char sym = 'X';
+	int isWinner = FALSE, winner = 0;
+	int flag = FALSE;
+	int rows = 0, cols = 0;
+
+	if (player-1)
+	{
+		sym = 'O';
+	}
+	putchar(sym);
+	//check main diag
+	for (rows = 0, cols = 0; !flag && rows < ROW_SIZE && cols < COLS; rows++, cols++)
+	{
+		if (board[rows][cols] != sym)
+		{
+			flag = TRUE;
+		}
+	}
+	if (!flag)
+	{
+		isWinner = TRUE;
+	}
+	else
+	{	//check sec diag
+		flag = FALSE;
+		for (rows = ROW_SIZE-1, cols = 0; !flag && rows >= 0 && cols < COLS; rows--, cols++)
+		{
+			if (board[rows][cols] != sym)
+			{
+				flag = TRUE;
+			}
+		}
+		if (!flag)
+		{
+			isWinner = TRUE;
+		}
+		else
+		{	//check by rows
+			flag = FALSE;
+			for (rows = 0; !isWinner && rows < ROW_SIZE; rows++)
+			{
+				for (cols = 0; !flag && cols < COLS; cols++)
+				{
+					if (sym != board[rows][cols])
+					{
+						flag = TRUE;
+					}
+				}
+				if (!flag)
+				{
+					isWinner = TRUE;
+				}
+			}
+			//check by cols
+			flag = FALSE;
+			for (cols = 0; !isWinner && cols < COLS; cols++)
+			{
+				flag = FALSE;
+				for (rows = 0; !flag && rows < ROW_SIZE; rows++)
+				{
+					if (sym != board[rows][cols])
+					{
+						flag = TRUE;
+					}
+				}
+				if (!flag)
+				{
+					isWinner = TRUE;
+				}
+			}
+		}
+	}
+	//call winner to stop game
+	if (isWinner)
+	{
+		winner = player;
+	}
+	return winner;
 }
 
 /*
@@ -81,27 +185,27 @@ int checkWinner(char board[][COLS])
 	input: gamaing board
 	output: 0 on success
 */
-int doTurn(char board[][COLS], int player)
+int doTurn(char board[][COLS], int player, const int  ROW_SIZE)
 {
 	int winner = 0;
 	int cell = 0, row = 0, col = 0;
 	int madeTurn = FALSE;
-	player %= NUM_PLAYERS;
+	player = player % NUM_PLAYERS + 1;
 	puts("player 1 you play X, player 2 you are O");
-	printf("player %d, it's your turn\n", player + 1);
+	printf("player %d, it's your turn\n", player);
 	do
 	{
-		puts("choose an empty cell, by click on a number, and make your move");
+		puts("choose an empty cell, by click on a number, and make your move: ");
 		cell = _getch();
 		putchar(cell);
 		if (cell >= '1' && cell <= '9') //check if is a number
 		{
 			cell = cell - '0';
-			row = ROWS - 1 - (cell-1) / ROWS;
+			row = ROW_SIZE - 1 - (cell-1) / ROW_SIZE;
 			col = (cell - 1) % COLS;
 			if (' ' == board[row][col]) //check if player chose an empty cell
 			{
-				if (player)
+				if (player-1)
 				{
 					board[row][col] = 'O';
 				}
@@ -111,17 +215,16 @@ int doTurn(char board[][COLS], int player)
 				}
 				//move to next player
 				madeTurn = TRUE;
-				player++;
 			}
 			else
 			{
-				printf("player %d,", player + 1);
+				printf("player %d,", player);
 				puts("this cell is not empty");
 			}
 		}
 		else
 		{
-			printf("player %d,", player + 1);
+			printf("player %d,", player);
 			puts("you didnt choose a number");
 		}
 	} while (!madeTurn);
@@ -133,16 +236,16 @@ int doTurn(char board[][COLS], int player)
 	input: two dim array - board
 	output: 0 on success
 */
-int displayBoard(char board[][COLS])
+int displayBoard(char board[][COLS], const int  ROW_SIZE)
 {
 	int rows = 0, cols = 0;
 	printf("-------------------\n");
-	for (rows = 0; rows < ROWS; rows++)
+	for (rows = 0; rows < ROW_SIZE; rows++)
 	{
 		putchar('|');
 		for (cols = 0; cols < COLS; cols++)
 		{
-			printf("    %d|", ROWS*(ROWS-rows)-COLS+cols+1);
+			printf("    %d|", ROW_SIZE *(ROW_SIZE -rows)-COLS+cols+1);
 		}
 		printf("\n|");
 		for (cols = 0; cols < COLS; cols++)
